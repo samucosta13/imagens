fb58cc77ec8798730a948abfd3f4bddf764a83b6
Samuel <samuelcosta.dev@gmail.com>
2022-10-22 12:09:44 -0300

    Update bandeira.py

--- bandeira.py
+++ bandeira.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 BRANCO = (255,255,255)
-def tricolor_horizontal(comprimento, altura):
+def tricolor_vertical(comprimento, altura):
     bandeira = Image.new("RGB", (comprimento,altura), BRANCO)
     cor = [BRANCO, BRANCO, BRANCO]
     print("Primeira cor: ")
@@ -28,6 +28,40 @@ def tricolor_horizontal(comprimento, altura):
                 j += 1
     bandeira.show()
 
+def tricolor_horizontal(comprimento, altura):
+    bandeira = Image.new("RGB", (comprimento,altura), BRANCO)
+    cor = [BRANCO, BRANCO, BRANCO]
+    print("Primeira cor: ")
+    cor[0] = [int(c) for c in input().split()]
+    print("Segunda cor:")
+    cor[1] = [int(c) for c in input().split()]
+    print("Terceira cor:")
+    cor[2] = [int(c) for c in input().split()]
+    for j in range(altura):
+        if j < altura/3:
+            i = 0
+            while i < comprimento:
+                bandeira.putpixel((i,j),(cor[0][0],cor[0][1],cor[0][2]))
+                i += 1
+        if j >= altura/3 and j < (2*altura/3):
+            i = 0
+            while i < comprimento:
+                bandeira.putpixel((i,j),(cor[1][0],cor[1][1],cor[1][2]))
+                i += 1
+        if j >= (2*altura/3) and j < altura:
+            i = 0
+            while i < comprimento:
+                bandeira.putpixel((i,j),(cor[2][0],cor[2][1],cor[2][2]))
+                i += 1
+    bandeira.show()
+
+print("BANDEIRAS: 1 - Faixas verticais, 2 - Faixas horizontais")
+tipo = int(input("Digite: "))
+
 a = int(input("Digite o comprimento: "))
 b = int(input("Digite a altura: "))
-tricolor_horizontal(a,b)
+
+if tipo == 1:
+    tricolor_vertical(a,b)
+elif tipo == 2:
+    tricolor_horizontal(a,b)
